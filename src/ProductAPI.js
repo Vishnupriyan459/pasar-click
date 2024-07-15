344648f23521a59fe87e2eb3b07d86530ae2d66e
vp@work <59786502+Vishnupriyan459@users.noreply.github.com>
2024-07-15 09:22:32 +0530

    Update ProductAPI.js

--- src/ProductAPI.js
+++ src/ProductAPI.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export const fetchProducts = async () => {
     try {
-      const response = await axios.get('/data/products.json');
+      const response = await axios.get('/Data/products.json');
       return response.data.products; // Access the array under the "vendor" key
     } catch (error) {
       console.error('Error fetching vendor data:', error);
